ee99eaf9a49596bcd3e0895d195e738aa4455ab2
dekos228 <dekos589191@gmail.com>
2024-05-31 20:29:23 +0200

    Create 0140-basic_arith.cpp

---
+++ 0140-basic_arith.cpp
@@ -0,0 +1,18 @@
+﻿// basic_arith.cpp
+#include <iostream>
+#include <cstdlib>
+
+int main()
+{
+  using namespace std;
+
+  int a = 0, b = 0; // Целые числа.
+  cout << "a = ";
+  cin >> a;
+  cout << "b = ";
+  cin >> b;
+
+  cout << "(a + 2b) (a - 2b) = ";
+  cout << (a + 2*b) * (a - 2*b) << endl;
+  return EXIT_SUCCESS;
+}
